da94aa017c1b9334c36d713061adfe340c637131
Mhdrzn <103956396+Mhdrzn@users.noreply.github.com>
2022-04-27 19:34:38 +0530

    Mor echanges and fix

--- plugins/start.py
+++ plugins/start.py
@@ -4,70 +4,55 @@ from pyrogram.errors import UserNotParticipant
 import humanize
 from Translation import mr
 from helper.database import  insert 
-from bot import FORCE_SUB
 
 
 @Client.on_message(filters.private & filters.command(["start"]))
-async def start(client,message):
-    if FORCE_SUB:   
-        try:             
-            user = await client.get_chat_member(FORCE_SUB, message.from_user.id)
-            if user.status == "kicked":
-               await message.reply_text("Sorry, You're Banned")
-               return
-        except UserNotParticipant:
-            await message.reply_text(
-                text="**sorry bro നിങ്ങൾ ഞങ്ങളുടെ ചാനലിൽ ജോയിൻ ചെയ്തിട്ടില്ല താഴെയുള്ള ബട്ടനിൽ ക്ലിക്ക് ചെയ്ത് join ചെയ്യൂ എന്നിട്ട് വീണ്ടും start കൊടുക്കൂ 🙏**",
-                reply_markup=InlineKeyboardMarkup([
-                    [ InlineKeyboardButton(text="📢𝙹𝚘𝚒𝚗 𝙼𝚢 𝚄𝚙𝚍𝚊𝚝𝚎 𝙲𝚑𝚊𝚗𝚗𝚎𝚕📢", url=f"https://t.me/{FORCE_SUB}")]
-              ])
-            )
-            return
-    else:
-        insert(int(message.chat.id))
-        await message.reply_photo(
-            photo="https://telegra.ph/file/2e2a07e86066538ed7406.jpg",
-            caption=f"""👋 Hai {message.from_user.mention} \n𝙸'𝚖 𝙰 𝚂𝚒𝚖𝚙𝚕𝚎 𝙵𝚒𝚕𝚎 𝚁𝚎𝚗𝚊𝚖𝚎+𝙵𝚒𝚕𝚎 𝚃𝚘 𝚅𝚒𝚍𝚎𝚘 𝙲𝚘𝚟𝚎𝚛𝚝𝚎𝚛 𝙱𝙾𝚃 𝚆𝚒𝚝𝚑 𝙿𝚎𝚛𝚖𝚊𝚗𝚎𝚗𝚝 𝚃𝚑𝚞𝚖𝚋𝚗𝚊𝚒𝚕 𝚂𝚞𝚙𝚙𝚘𝚛𝚝! \n𝙱𝙾𝚃 𝙲𝚛𝚎𝚊𝚝𝚎𝚍 𝙱𝚢: @mr_MKN & @Mr_MKN_TG \n 🤩""",
-            reply_markup=InlineKeyboardMarkup( [[
-                InlineKeyboardButton("👨‍💻 OWNER 👨‍💻", url='https://t.me/mr_MKN')
-                ],[
-                InlineKeyboardButton('📢 UPDATES', url='https://t.me/mkn_bots_updates'),
-                InlineKeyboardButton('ℹ️ SUPPORT', url='https://t.me/MKN_BOTZ_DISCUSSION_GROUP')
-                ],[
-                InlineKeyboardButton('🛡️ About', callback_data='about'),
-                InlineKeyboardButton('ℹ️ Help', url='https://t.me/mrmoviesseries_print')
-                ]]
-                )
-            )
+async def start(client, message):
+    if not await is_subscribed(client, message):
+       await message.reply_text(
+       text="**sorry bro നിങ്ങൾ ഞങ്ങളുടെ ചാനലിൽ ജോയിൻ ചെയ്തിട്ടില്ല താഴെയുള്ള ബട്ടനിൽ ക്ലിക്ക് ചെയ്ത് join ചെയ്യൂ എന്നിട്ട് വീണ്ടും start കൊടുക്കൂ 🙏**",
+       reply_markup=InlineKeyboardMarkup([
+           [ InlineKeyboardButton(text="📢𝙹𝚘𝚒𝚗 𝙼𝚢 𝚄𝚙𝚍𝚊𝚝𝚎 𝙲𝚑𝚊𝚗𝚗𝚎𝚕📢", url=f"https://t.me/{client.invitelink}")]
+           ])
+       )
+       return
+    insert(int(message.chat.id))
+    await message.reply_photo(
+       photo="https://telegra.ph/file/2e2a07e86066538ed7406.jpg",
+       caption=f"""👋 Hai {message.from_user.mention} \n𝙸'𝚖 𝙰 𝚂𝚒𝚖𝚙𝚕𝚎 𝙵𝚒𝚕𝚎 𝚁𝚎𝚗𝚊𝚖𝚎+𝙵𝚒𝚕𝚎 𝚃𝚘 𝚅𝚒𝚍𝚎𝚘 𝙲𝚘𝚟𝚎𝚛𝚝𝚎𝚛 𝙱𝙾𝚃 𝚆𝚒𝚝𝚑 𝙿𝚎𝚛𝚖𝚊𝚗𝚎𝚗𝚝 𝚃𝚑𝚞𝚖𝚋𝚗𝚊𝚒𝚕 𝚂𝚞𝚙𝚙𝚘𝚛𝚝! \n𝙱𝙾𝚃 𝙲𝚛𝚎𝚊𝚝𝚎𝚍 𝙱𝚢: @mr_MKN & @Mr_MKN_TG \n 🤩""",
+       reply_markup=InlineKeyboardMarkup( [[
+          InlineKeyboardButton("👨‍💻 OWNER 👨‍💻", url='https://t.me/mr_MKN')
+          ],[
+          InlineKeyboardButton('📢 UPDATES', url='https://t.me/mkn_bots_updates'),
+          InlineKeyboardButton('ℹ️ SUPPORT', url='https://t.me/MKN_BOTZ_DISCUSSION_GROUP')
+          ],[
+          InlineKeyboardButton('🛡️ About', callback_data='about'),
+          InlineKeyboardButton('ℹ️ Help', url='https://t.me/mrmoviesseries_print')
+          ]]
+          )
+       )
     return
 
 @Client.on_message(filters.private &( filters.document | filters.audio | filters.video ))
-async def send_doc(client,message):
-    if FORCE_SUB:   
-        try:             
-            user = await client.get_chat_member(FORCE_SUB, message.from_user.id)
-            if user.status == "kicked":
-               await message.reply_text("Sorry, You're Banned")
-               return
-        except UserNotParticipant:
-            await message.reply_text(
-                text="**sorry bro നിങ്ങൾ ഞങ്ങളുടെ ചാനലിൽ ജോയിൻ ചെയ്തിട്ടില്ല താഴെയുള്ള ബട്ടനിൽ ക്ലിക്ക് ചെയ്ത് join ചെയ്യൂ എന്നിട്ട് വീണ്ടും start കൊടുക്കൂ 🙏**",
-                reply_markup=InlineKeyboardMarkup([
-                    [ InlineKeyboardButton(text="📢𝙹𝚘𝚒𝚗 𝙼𝚢 𝚄𝚙𝚍𝚊𝚝𝚎 𝙲𝚑𝚊𝚗𝚗𝚎𝚕📢", url=f"https://t.me/{FORCE_SUB}")]
-              ])
-            )
-            return
+async def send_doc(client, message):
+    if not await is_subscribed(client, message):
+       await message.reply_text(
+       text="**sorry bro നിങ്ങൾ ഞങ്ങളുടെ ചാനലിൽ ജോയിൻ ചെയ്തിട്ടില്ല താഴെയുള്ള ബട്ടനിൽ ക്ലിക്ക് ചെയ്ത് join ചെയ്യൂ എന്നിട്ട് വീണ്ടും start കൊടുക്കൂ 🙏**",
+       reply_markup=InlineKeyboardMarkup([
+           [ InlineKeyboardButton(text="📢𝙹𝚘𝚒𝚗 𝙼𝚢 𝚄𝚙𝚍𝚊𝚝𝚎 𝙲𝚑𝚊𝚗𝚗𝚎𝚕📢", url=f"https://t.me/{client.invitelink}")]
+           ])
+       )
+       return
     else:
-        media = await client.get_messages(message.chat.id,message.id)
-        file = media.document or media.video or media.audio 
-        filename = file.file_name
-        filesize = humanize.naturalsize(file.file_size)
-        fileid = file.file_id
-        await message.reply_text(
-            f"__What do you want me to do with this file?__\n**File Name** :- {filename}\n**File Size** :- {filesize}",
-            reply_to_message_id = message.id,
-            reply_markup = InlineKeyboardMarkup([[ InlineKeyboardButton("📝 Rename ",callback_data = "rename"),
-            InlineKeyboardButton("Cancel✖️",callback_data = "cancel")  ]]))
+       file = message.document or message.video or message.audio 
+       filename = file.file_name
+       filesize = humanize.naturalsize(file.file_size)
+       fileid = file.file_id
+       await message.reply_text(
+           f"__What do you want me to do with this file?__\n**File Name** :- {filename}\n**File Size** :- {filesize}",
+           reply_to_message_id = message.id,
+           reply_markup = InlineKeyboardMarkup([[ InlineKeyboardButton("📝 Rename ",callback_data = "rename"),
+           InlineKeyboardButton("Cancel✖️",callback_data = "cancel")  ]]))
 
 
 @Client.on_callback_query()
@@ -89,7 +74,14 @@ async def cb_handler(client, query: CallbackQuery):
         except:
             pass
 
-
-
-
-
+async def is_subscribed(client, message):
+   if not client.force_sub:
+      return True
+   try:             
+      user = await client.get_chat_member(client.force_sub, message.from_user.id)
+   except UserNotParticipant:
+      pass
+   else:
+      if user.status != "kicked":
+         return True 
+   return False
