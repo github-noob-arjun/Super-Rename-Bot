b92adcbb82c25a96e185810b0c428e69b4b4f7fb
Mhdrzn <103956396+Mhdrzn@users.noreply.github.com>
2022-04-27 14:59:55 +0530

    Pyrogram v.2.0.0

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            session_name="renamer",
+            name="renamer",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
