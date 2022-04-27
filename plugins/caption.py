c5c027dd489f0d4df8d72cce518eaf99a1434862
Mhdrzn <103956396+Mhdrzn@users.noreply.github.com>
2022-04-27 14:58:19 +0530

    Custom caption

--- plugins/caption.py
+++ plugins/caption.py
@@ -1,7 +1,25 @@
 from pyrogram import Client, filters 
+from helper.database import find, addcaption, delcaption 
 
 @Client.on_messages(filters.private & filters.command('set_caption'))
 async def add_caption(client, message):
     if len(message.command) == 1:
-       return await message.reply_text("**Give me a caption to set.\n\nExample:- `/set_caption 📕 File Name: {filename}\n\n💾 Size: {filesize}\n\n⏰ Duration: {duration}`**)
-    
+       return await message.reply_text("**Give me a caption to set.\n\nExample:- `/set_caption 📕 File Name: {filename}\n\n💾 Size: {filesize}\n\n⏰ Duration: {duration}`**")
+    addcaption(int(message.chat.id), message.command[1])
+    await message.reply_text("**Your Caption successfully added ✅**")
+
+@Client.on_messages(filters.private & filters.command('del_caption'))
+async def delete_caption(client, message): 
+    caption = fint(int(message.chat.id))[1]
+    if not caption:
+       return await message.reply_text("**You dont have any custom caption**")
+    delcaption(int(message.chat.id))
+    await message.reply_text("**Your caption successfully deleted ✅**")
+                                       
+@Client.on_messages(filters.private & filters.command('see_caption'))
+async def see_caption(client, message): 
+    caption = find(int(message.chat.id))[1]
+    if caption:
+       await message.reply_text(f"<b><u>Your Caption:</b></u>\n\n`{caption}`")
+    else:
+       await message.reply_text("**You dont have any custom caption**")
