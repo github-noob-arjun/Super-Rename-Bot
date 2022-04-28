f7617cb6110737f63cf0064e7c0976b5f3cfb3de
Mhdrzn <103956396+Mhdrzn@users.noreply.github.com>
2022-04-28 15:12:38 +0530

    Fix

--- plugins/filedetect.py
+++ plugins/filedetect.py
@@ -5,22 +5,22 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceRepl
 async def refunc(client,message):
     reply_message = message.reply_to_message
     if (reply_message.reply_markup) and isinstance(reply_message.reply_markup, ForceReply):
-    	new_name = message.text
-     	await message.delete() 
-        await message.reply_to_message.delete()
-        media = getattr(message, reply_message.media)
-        try:
-            new_file_name = new_name.split(".")
-        except:
-            return await message.reply_text(
-                      "**Error** :  No  Extension in File, Not Supporting",
+       new_name = message.text 
+       await message.delete() 
+       await message.reply_to_message.delete()
+       media = getattr(message, reply_message.media)
+       try:
+           check = new_name.split(".") 
+       except:
+           return await message.reply_text(
+                     "**Error** :  No  Extension in File, Not Supporting",
                       reply_to_message_id=reply_message.id)
-        button = [[InlineKeyboardButton("📁 Documents",callback_data = "doc")]]
-      	if media == "video":
-            button[-1].append(InlineKeyboardButton("🎥 Video",callback_data = "vid"))
-        elif media == "audio":
-            button[-1].append(InlineKeyboardButton("🎵 audio",callback_data = "aud"))
-        await message.reply_text(
-           f"**Select the output file type**\n**Output FileName** :- ```{new_file_name}```",
-           reply_to_message_id=reply_message.id,
-           reply_markup=InlineKeyboardMarkup(button))
+       button = [[InlineKeyboardButton("📁 Documents",callback_data = "doc")]]
+       if media == "video":
+           button[-1].append(InlineKeyboardButton("🎥 Video",callback_data = "vid"))
+       elif media == "audio":
+           button[-1].append(InlineKeyboardButton("🎵 audio",callback_data = "aud"))
+       await message.reply_text(
+          f"**Select the output file type**\n**Output FileName** :- ```{new_name}```",
+          reply_to_message_id=reply_message.id,
+          reply_markup=InlineKeyboardMarkup(button))
