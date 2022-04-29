ce7bf046854c14576fe9eeebfd7635b0385fc0a5
Mhdrzn <103956396+Mhdrzn@users.noreply.github.com>
2022-04-29 06:26:54 +0530

    Update cb_data.py

--- plugins/cb_data.py
+++ plugins/cb_data.py
@@ -41,27 +41,29 @@ async def doc(bot,update):
      	path = await bot.download_media(message = file, progress=progress_for_pyrogram,progress_args=( "``` Trying To Download...```",  ms, c_time   ))
      except Exception as e:
      	await ms.edit(e)
-     	return
+     	return 
+     await ms.edit("download completed")
      splitpath = path.split("/downloads/")
      dow_file_name = splitpath[1]
      old_file_name =f"downloads/{dow_file_name}"
-     os.rename(old_file_name,file_path) 
+     os.rename(old_file_name,file_path)
+     await ms.edit("renamed")
      duration = 0
      metadata = extractMetadata(createParser(file_path))
      if metadata.has("duration"):
      	duration = metadata.get('duration').seconds
      user_id = int(update.message.chat.id) 
-     ph_path = None
-     find = find(user_id) 
-     try:
+     try: 
+	ph_path = None
+        data = find(user_id) 
         media = getattr(file, file.media.value)
-        c_caption = find[1]
+        c_caption = data[1]
         if c_caption:
             caption = c_caption.format(filename=new_filename, filesize=humanize.naturalsize(media.file_size), duration=duration)
         else:
             caption = f"**{new_filename}**"
-        if find[0]:
-           ph_path = await bot.download_media(find[0]) 
+        if data[0]:
+           ph_path = await bot.download_media(data[0]) 
            Image.open(ph_path).convert("RGB").save(ph_path)
            img = Image.open(ph_path)
            img.resize((320, 320))
