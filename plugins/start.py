e2a61b8cbb723e393a98e2552bdebafe7d94f04a
Mhdrzn <103956396+Mhdrzn@users.noreply.github.com>
2022-04-29 13:57:30 +0530

    some changes

--- plugins/start.py
+++ plugins/start.py
@@ -35,12 +35,12 @@ async def start(client, message):
 
 @Client.on_message(filters.private &( filters.document | filters.audio | filters.video ))
 async def send_doc(client, message):
-    file = message.document or message.video or message.audio 
+    file = getattr(message, message.media.value)
     filename = file.file_name
     filesize = humanize.naturalsize(file.file_size)
     fileid = file.file_id
     await message.reply_text(
-        f"__What do you want me to do with this file?__\n**File Name** :- {filename}\n**File Size** :- {filesize}",
+        f"__What do you want me to do with this file?__\n**File Name** :- `{filename}`\n**File Size** :- `{filesize}`",
         reply_to_message_id = message.id,
         reply_markup = InlineKeyboardMarkup([[ InlineKeyboardButton("📝 Rename ",callback_data = "rename"),
         InlineKeyboardButton("Cancel✖️",callback_data = "cancel")  ]]))
