44776a5ec1b7810dc40a88dbe6179f0e9996c552
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-07-10 11:42:36 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,9 +7,7 @@ logging.config.fileConfig("logging.conf")
 logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("pyrogram").setLevel(logging.ERROR) 
 
-#BOT_TOKEN = "5599652951:AAEcYOr9VYNGkjirLKp5HLELi1SfLF74G98" #os.environ.get("BOT_TOKEN", "")
-
-BOT_TOKEN = "5531033586:AAGpDB1McColbRyctemu3qyrC1iJEjdRVPI"
+BOT_TOKEN = "5599652951:AAEcYOr9VYNGkjirLKp5HLELi1SfLF74G98" #os.environ.get("BOT_TOKEN", "")
 
 API_ID = 6015447 #int(os.environ.get("API_ID", ""))
 
