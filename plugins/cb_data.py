43e1d62be38dd6c595ad3d81d1bec23826c8704c
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-08-08 20:45:54 +0530

    Update cb_data.py

--- plugins/cb_data.py
+++ plugins/cb_data.py
@@ -21,16 +21,18 @@ async def cancel(bot,update):
 async def rename(bot,update):
 	user_id = update.message.chat.id
 	date = update.message.date
-	#await update.message.delete()
+	await update.message.delete()
 	await update.message.reply_text("__𝙿𝚕𝚎𝚊𝚜𝚎 𝙴𝚗𝚝𝚎𝚛 𝙽𝚎𝚠 𝙵𝚒𝚕𝚎𝙽𝚊𝚖𝚎...__",	
 	reply_to_message_id=update.message.reply_to_message.id,  
 	reply_markup=ForceReply(True))
 	
-@Client.on_callback_query(filters.regex("upload"))
+#@Client.on_callback_query(filters.regex("upload"))
+@Client.on_message(filters.command("r"))
 async def doc(bot,update):
-     type = update.data.split('_')[1]
+     #type = update.data.split('_')[1]
      new_name = update.message.text
-     new_filename = new_name.split(":-")[1]
+     #new_filename = new_name.split(":-")[1]
+     new_filename = new_name.split(" ")[1]
      if not "!" in new_filename:
         new_filename = new_filename + ".mkv"
      else:
