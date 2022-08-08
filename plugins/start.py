e3380dddddfed18ab95016b9d49417c72f68e172
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-08-08 16:04:02 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -21,15 +21,7 @@ async def start(client, message):
        photo="https://telegra.ph/file/04d08445dce68c9a57b25.jpg",
        caption=f"""👋 Hai {message.from_user.mention} \n𝙸'𝚖 𝙰 𝚂𝚒𝚖𝚙𝚕𝚎 𝙵𝚒𝚕𝚎 𝚁𝚎𝚗𝚊𝚖𝚎+𝙵𝚒𝚕𝚎 𝚃𝚘 𝚅𝚒𝚍𝚎𝚘 𝙲𝚘𝚟𝚎𝚛𝚝𝚎𝚛 𝙱𝙾𝚃 𝚆𝚒𝚝𝚑 𝙿𝚎𝚛𝚖𝚊𝚗𝚎𝚗𝚝 𝚃𝚑𝚞𝚖𝚋𝚗𝚊𝚒𝚕 & 𝙲𝚞𝚜𝚝𝚘𝚖 𝙲𝚊𝚙𝚝𝚒𝚘𝚗 𝚂𝚞𝚙𝚙𝚘𝚛𝚝! """,
        reply_markup=InlineKeyboardMarkup( [[
-           InlineKeyboardButton("👼 𝙳𝙴𝚅𝚂 👼", callback_data='dev')
-           ],[
-           InlineKeyboardButton("❤️‍🔥 𝚆𝙰𝚃𝙲𝙷 𝚃𝚄𝚃𝙾𝚁𝙸𝙰𝙻 ❤️‍🔥", url='https://youtu.be/BiC66uFJsio')
-           ],[
-           InlineKeyboardButton('📢 𝚄𝙿𝙳𝙰𝚃𝙴𝚂', url='https://t.me/PYRO_BOTZ'),
-           InlineKeyboardButton('🍂 𝚂𝚄𝙿𝙿𝙾𝚁𝚃', url='https://t.me/PYRO_BOTZ_CHAT')
-           ],[
-           InlineKeyboardButton('🍃 𝙰𝙱𝙾𝚄𝚃', callback_data='about'),
-           InlineKeyboardButton('ℹ️ 𝙷𝙴𝙻𝙿', callback_data='help')
+           InlineKeyboardButton("💠 EDITOR 💠", url='https://t.me/github_noob')
            ]]
           )
        )
