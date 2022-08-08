ba853a426f89a163317b129086f5c07d422dac02
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-08-08 23:21:06 +0530

    Delete start.py

--- plugins/start.py
+++
@@ -1,6 +0,0 @@
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply, CallbackQuery
-import humanize
-from helper.txt import mr
-from helper.database import  insert 
-
