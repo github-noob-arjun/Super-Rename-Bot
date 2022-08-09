abb396cfbe4bee7de64286e75c839f45561e43ce
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-08-09 12:29:37 +0530

    Update thumbfuc.py

--- plugins/thumbfuc.py
+++ plugins/thumbfuc.py
@@ -4,7 +4,7 @@ from helper.database import find, delthumb, addthumb
 @Client.on_message(filters.group & filters.command(['viewthumb']))
 async def viewthumb(client,message):
     try:
-        thumb, _ = find(int(message.from_user.id))[0][1]
+        thumb = find(int(message.from_user.id))[0][1]
         try:
             if thumb:
                 await message.reply_photo(photo=thumb)
